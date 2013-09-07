5cb57bcc7cdae4609e8276e5c11b0fcb0db34c95
Nathan Osman <admin@quickmediasolutions.com>
2013-09-07 11:27:18 -0700

    Initial Commit

---
+++ src/com/nathanosman/c2/MessageListActivity.java
@@ -0,0 +1,15 @@
+package com.nathanosman.c2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MessageListActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
